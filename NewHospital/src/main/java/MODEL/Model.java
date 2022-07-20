3049c737796ffe12daf59cf46061bd4e313f560f
thanh1062 <87410424+thanh1062@users.noreply.github.com>
2022-07-20 14:42:22 +0700

    Add files via upload

---
+++ NewHospital/src/main/java/MODEL/Model.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MODEL;
+
+/**
+ *
+ * @author yukth
+ */
+public class Model {
+    private int ID;
+    private int nh;
+}
